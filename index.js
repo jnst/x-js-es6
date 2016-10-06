cce927742622bdcffdf0c8ac27da74eefd02e114
jnst <jnst@outlook.com>
2016-10-06 10:25:35 +0900

    Change print timing

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const moment = require('moment');
 const cron = require('node-cron');
-
 const apple = require('./lib/apple');
-const logger = require('./lib/logger');
 
-const task = cron.schedule('0,30 * * * * *', () => {
-  apple.scan();
-  logger.info(moment().format('YYYY-MM-DD HH:mm:ss'));
-});
-task.start();
+cron.schedule('0,30 * * * * *', apple.scan, true).start();
